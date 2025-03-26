10129ed0cb4b982197a0d16c10b494c865dcf2b5
developharsh <harsh237hk@gmail.com>
2025-03-27 01:35:07 +0530

    changed node module and implemented cors for local and given website

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: ["https://e-commerce-revispy.vercel.app", "http://localhost:3000"], 
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
